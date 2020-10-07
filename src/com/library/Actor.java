7c1547f9587e7e56736b874ff0e0b86dc0eacdde
Hampus Nilsson <hampus10699@gmail.com>
2020-10-08 00:55:06 +0200

    removed unused methods.

--- src/com/library/Actor.java
+++ src/com/library/Actor.java
@@ -5,22 +5,8 @@ import java.util.ArrayList;
 
 public class Actor extends Person implements Serializable, Comparable<Actor> {
 
-    private ArrayList<Movie> movies = new ArrayList<Movie>();
-
     public Actor(String firstName, String lastName, int age) {
         super(firstName, lastName, age);
-    }
-
-    public void addMovie(Movie movie){
-        movies.add(movie);
-    }
-
-    public Movie getMovie(int index){
-        return movies.get(index);
-    }
-
-    public ArrayList<Movie> getMovies(){
-        return movies;
     }
 
     @Override
