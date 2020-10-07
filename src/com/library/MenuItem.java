89c59e89749b27bb4bcf83d95534e1bd9edafef1
Hampus Nilsson <hampus10699@gmail.com>
2020-10-07 22:10:04 +0200

    added customer menu choices in menuItem enum

--- src/com/library/MenuItem.java
+++ src/com/library/MenuItem.java
@@ -3,22 +3,24 @@ package com.library;
 public enum MenuItem {
 
     SHOW_ALL_ACTORS("1. Show all actors"),
-    SHOW_ALL_MOVIES("2. Show all movies"),
-    SHOW_ALL_PRODUCTION_COMPANIES("3. Show all production companies"),
-    ADD_NEW_ACTOR("4. Add new actor"),
-    ADD_NEW_MOVIE("5. Add new movie"),
-    ADD_NEW_PRODUCTION_COMPANY("6. Add new production company"),
-    SHOW_ACTOR("7. Show actor"),
-    SHOW_MOVIE("8. Show movie"),
-    SHOW_PRODUCTION_COMPANY("9. Show production company"),
-    REMOVE_ACTOR("10. Remove actor"),
-    REMOVE_MOVIE("11. Remove movie"),
-    REMOVE_PRODUCTION_COMPANY("12. Remove production company"),
-    SORT_ACTOR_LIST("13. Sort actors list"),
-    SORT_MOVIE_LIST("14. Sort movies list"),
-    SORT_PRODUCTION_COMPANY_LIST("15. Sort production company list"),
-    HELP_MENU("16. Show help menu"),
-    EXIT_PROGRAM("17. Exit Movie Library Program");
+    SHOW_ALL_CUSTOMERS("2. Show all customers"),
+    SHOW_ALL_MOVIES("3. Show all movies"),
+    SHOW_ALL_PRODUCTION_COMPANIES("4. Show all production companies"),
+    ADD_NEW_ACTOR("5. Add new actor"),
+    ADD_NEW_CUSTOMER("6. Add new customer"),
+    ADD_NEW_MOVIE("7. Add new movie"),
+    ADD_NEW_PRODUCTION_COMPANY("8. Add new production company"),
+    SHOW_ACTOR("9. Show actor"),
+    SHOW_CUSTOMER("10. Show customer"),
+    SHOW_MOVIE("11. Show movie"),
+    SHOW_PRODUCTION_COMPANY("12. Show production company"),
+    REMOVE_ACTOR("13. Remove actor"),
+    REMOVE_CUSTOMER("14. Remove customer"),
+    REMOVE_MOVIE("15. Remove movie"),
+    REMOVE_PRODUCTION_COMPANY("16. Remove production company"),
+    SORT_ACTOR_LIST("17. Sort actors list"),
+    HELP_MENU("18. Show help menu"),
+    EXIT_PROGRAM("19. Exit Movie Library Program");
 
     public String description;
 
